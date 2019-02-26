b4403774092874ba3a3449e251d8ac2adb4a71e1
Alexandra <alexis07oana@yahoo.com>
2019-02-26 17:33:33 +0200

    Used gradle, made main function and added a test

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class MyTest {
+    @Test
+    public void name() throws Exception
+    {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Main.print(new PrintStream(out));
+        String s =out.toString();
+        Assert.assertEquals("Hello World!", s);
+    }
+}
