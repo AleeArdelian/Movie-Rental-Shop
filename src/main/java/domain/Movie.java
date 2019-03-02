8098bcc16ae1cd018f8313b8d3a331b28824ce96
Alexandra <alexis07oana@yahoo.com>
2019-03-03 01:08:24 +0200

    Added Movie repository and Service

---
+++ src/main/java/domain/Movie.java
@@ -0,0 +1,31 @@
+package domain;
+
+public class Movie extends BaseEntity<Integer> {
+
+    private String movieName;
+    private Integer yearOFRelease;
+    private String regizor;
+
+    public Movie(String name, Integer year, String reg)
+    {
+        this.movieName=name;
+        this.yearOFRelease=year;
+        this.regizor=reg;
+    }
+
+    public String getMovieName()
+    {
+        return this.movieName;
+    }
+
+    public Integer getYear()
+    {
+        return this.yearOFRelease;
+    }
+
+    public String getMovieRegizor()
+    {
+        return this.regizor;
+    }
+
+}
---
+++ src/main/java/service/StudentRentalService.java
@@ -0,0 +1,13 @@
+package service;
+
+import domain.Client;
+import domain.Movie;
+import repository.Repository;
+
+public class StudentRentalService {
+
+    private Repository<Integer, Client> clientRepository;
+    private Repository<Integer, Movie> movieRepository;
+
+    //public StudentRentalService(Repository<Integer,Client> )
+}
