aa6b987d4a670ed9f809cb74e729a01a84ce9905
Alexandra <alexis07oana@yahoo.com>
2019-03-03 15:04:32 +0200

    Added Controller and UI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,13 @@ public class Main {
     public static void main(String[] args) {
         Validator<Client> clientValidator = new ClientValidator();
         Repository<Integer, Client> clientRepository = new InMemoryRepository<>(clientValidator);
+        Client client1= new Client("Alexandru","Balea", 20);
+        client1.setId(1);
+        clientRepository.save(client1);
+        Client client2= new Client("Alexandra","Ardelian", 20);
+        client2.setId(2);
+        clientRepository.save(client2);
+
         ClientRentalService crs = new ClientRentalService(clientRepository);
         UI consoleUI = new UI(crs);
         consoleUI.start();
