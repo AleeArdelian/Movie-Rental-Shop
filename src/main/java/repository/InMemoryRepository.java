bdf51e5f1c3250c852b60da0effef98fbb26e00f
Alexandra <alexis07oana@yahoo.com>
2019-03-04 19:24:48 +0200

    Comments for the repository

--- src/main/java/repository/InMemoryRepository.java
+++ src/main/java/repository/InMemoryRepository.java
@@ -30,6 +30,11 @@ public class InMemoryRepository<ID, T extends BaseEntity<ID>> implements Reposit
         this.validator = validator;
     }
 
+    /**
+     * Object of type T if the id exists in the repository, null otherwise
+     * @param id a {@code Integer} representing the object`s id
+     * @return a {@code Object of type T} / null
+     */
     @Override
     public Optional<T> findOne(ID id) {
         if (id == null) {
@@ -38,12 +43,23 @@ public class InMemoryRepository<ID, T extends BaseEntity<ID>> implements Reposit
         return Optional.ofNullable(entities.get(id));
     }
 
+    /**
+     * Create a set of all the entities found in the repository
+     * @return a {@code Set} of all the entities of type T
+     */
+
     @Override
     public Iterable<T> findAll() {
         Set<T> allEntities = entities.entrySet().stream().map(entry -> entry.getValue()).collect(Collectors.toSet());
         return allEntities;
     }
 
+    /**
+     *  Adds the entity object to the repository
+     * @param entity an object of type T
+     * @throws ValidatorException if @param entity is not valid
+     * @return a {@code Map} with the entity object added
+     */
     @Override
     public Optional<T> save(T entity) throws ValidatorException {
         if (entity == null) {
@@ -53,6 +69,12 @@ public class InMemoryRepository<ID, T extends BaseEntity<ID>> implements Reposit
         return Optional.ofNullable(entities.putIfAbsent(entity.getId(), entity));
     }
 
+    /**
+     * Deletes the object of type T from the repository if the id exists
+     * @param id a {@code Integer}
+     * @throws IllegalArgumentException if the id is null
+     * @return a {@code Map} without the object with the given id
+     */
     @Override
     public Optional<T> delete(ID id) {
         if (id == null) {
@@ -61,6 +83,13 @@ public class InMemoryRepository<ID, T extends BaseEntity<ID>> implements Reposit
         return Optional.ofNullable(entities.remove(id));
     }
 
+    /**
+     * Update the entity object found in the repository
+     * @param entity an object of type T
+     * @throws IllegalArgumentException if @@id is null
+     * @throws ValidatorException if @entity is not valid
+     * @return a {@code Map} with the entity object modified
+     */
     @Override
     public Optional<T> update(T entity) throws ValidatorException {
         if (entity == null) {
